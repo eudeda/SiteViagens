4eac82b48d196bf454329158f10a2064f056fcac
Louanderson Santos <louanderson7@hotmail.com>
2024-07-24 12:42:08 -0300

    Contact section done.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,6 @@ module.exports = {
       fontFamily: {
         'Poppins': ['"Poppins", "sans-serif;"'],
       },
-      keyframes: {
-        sideLoad: {
-          '0%': { width: '0%'},
-          '100%': { width: '100%' }
-        }
-      }
     },
   },
   plugins: [],
